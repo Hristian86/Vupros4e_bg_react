85909f3ad084dc97ffd4c75fa938308d9da5bc5c
Hristian86 <59567820+Hristian86@users.noreply.github.com>
2020-11-11 17:18:02 +0200

    new forum

--- src/Pages/Comments/CommentModel.js
+++ src/Pages/Comments/CommentModel.js
@@ -2,22 +2,35 @@ import React from 'react';
 
 const CommentModel = ({ content, title, createdOn, userName }) => {
 
-    return <div className="container">
-        <div className="media pl-sm-5 ml-sm-5 mt-5">
-            
-                <div className="media-body"  >
+    return <div className="container mt-1">
+        <div className="row">
+            <div className="col-6">
+            <i className="fa fa-user pt-1"></i> {userName}
+            </div>
+            <div className="col-6 text-right">
+            <i className="fa fa-calendar pt-1 text-right" aria-hidden="true"></i> {new Date(createdOn).toUTCString()}
+            </div>
+        </div>
+        <div className="media pl-sm-5 ml-sm-5 mt-2">
+
+
+            <div className="media-body row">
                 {/*<h1 className="mt-0">{title}</h1>*/}
                 <div dangerouslySetInnerHTML={{ __html: content }} />
-                    <br />
-                    <i className="fa fa-calendar pt-1" aria-hidden="true"></i> {new Date(createdOn).toUTCString()}
+
+                {/*<div className="col-7">
+                </div>*/}
+
+
+                {/*<div className="text-right col-5">
+                    
+                </div>*/}
             </div>
 
-            <div className="">
-                <i className="fa fa-user pt-1"></i> {userName}
-            </div>
-            </div>
 
-            <hr />
+        </div>
+
+        <hr />
     </div>
 }
 

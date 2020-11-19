b9267c6066c4e7205a0c48987e602beca211a142
Hristian86 <59567820+Hristian86@users.noreply.github.com>
2020-11-19 15:02:27 +0200

    Change color

--- src/components/Header/HeaderLogo.js
+++ src/components/Header/HeaderLogo.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Header.css';
 
 const HeaderLogo = () => {
-    return <div className="text-center pt-md-3 pb-md-3 bg-light">
-        <h1>Vyprosè.bg</h1>
+    return <div className="text-center header__logos pt-md-3 pb-md-3 bg-light">
+        <h1 className="text-danger">Vypros4e.bg</h1>
     </div>
 }
 
